33a0bedc8e6b25523c7d6bcae04cc7871bececfd
ruby <sergioloimpo@gmail.com>
2024-03-01 20:24:07 -0500

    actualizacion punto 2

--- punto2/funciones2.js
+++ punto2/funciones2.js
@@ -1,34 +1,61 @@
-function consumirApi(){
+function consumirApi() {
     var endPoint = document.getElementById("api").value;
     fetch(endPoint)
-    .then(function(response){
-        return response.json()
-    })
-    .then(function(data){
-        var poblacionEurope = 0;
-        var poblacionAsia = 0;
-        var poblacionSouthAmerica = 0; // Inicializa la población de Sudamérica en 0
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+        // creamos arreglos para cada pais
+            var paisesEurope = [];
+            var paisesAsia = [];
+            var paisesSouthAmerica = [];
+// Esto itera sobre cada elemento en el arreglo data, que contiene los datos de los países de la API
+            data.forEach(function(country) {
+                if (country.region === "Europe") {
+                    //dividimos los paises por region y luego usamos el metodo push aprendido en clase para agregar elementos al arreglo 
+                    paisesEurope.push({
+                        nombre: country.name.common,
+                        poblacion: country.population
+                    });
+                } else if (country.region === "Asia") {
+                    paisesAsia.push({
+                        nombre: country.name.common,
+                        poblacion: country.population
+                    });
+                } else if (country.subregion === "South America") {
+                    paisesSouthAmerica.push({
+                        nombre: country.name.common,
+                        poblacion: country.population
+                    });
+                }
+            });
+//usamos el metodo map para mapear iterar sobre cada elementos en este caso sobre las caracteristicas 
+//escogidas en el item anterior
+            var datosEurope = {
+                x: paisesEurope.map(function(pais) { return pais.nombre; }),
+                y: paisesEurope.map(function(pais) { return pais.poblacion; }),
+                type: 'bar',
+                name: 'Europe'
+            };
 
-        for(var i = 0; i < data.length; i++){
-            if(data[i].subregion === "South America"){ // Verifica la subregión en lugar de la región
-                poblacionSouthAmerica += data[i].population; // Suma la población de cada país en Sudamérica
-            } else if(data[i].region === "Europe"){
-                poblacionEurope += data[i].population;
-            } else if(data[i].region === "Asia"){
-                poblacionAsia += data[i].population;
-            }
-        }
+            var datosAsia = {
+                x: paisesAsia.map(function(pais) { return pais.nombre; }),
+                y: paisesAsia.map(function(pais) { return pais.poblacion; }),
+                type: 'bar',
+                name: 'Asia'
+            };
 
-        var grafica = [
-            {
-                x: ['Europe', 'Asia', 'South America'],
-                y: [poblacionEurope, poblacionAsia, poblacionSouthAmerica],
-                type: 'bar'
-            }
-        ];
-        Plotly.newPlot('myDiv', grafica);
-    })
-    .catch(function(error){
-        console.log(error);
-    })
+            var datosSouthAmerica = {
+                x: paisesSouthAmerica.map(function(pais) { return pais.nombre; }),
+                y: paisesSouthAmerica.map(function(pais) { return pais.poblacion; }),
+                type: 'bar',
+                name: 'South America'
+            };
+// layout Es el objeto de configuración de diseño que define cómo se verá la gráfica
+            var layout = {barmode: 'group'};
+            Plotly.newPlot('myDiv', [datosEurope, datosAsia, datosSouthAmerica], layout);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
